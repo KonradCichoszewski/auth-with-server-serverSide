abe9d226c7e7a5f9ff3659024b33b3163cf57307
Konrad <cichoszewicz@gmail.com>
2020-05-05 23:20:59 +0200

    minor corrections

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const expressCors = require('express-cors');
 
 const router = require('./router');
 
@@ -12,11 +13,14 @@ mongoose.connect('mongodb://localhost:27017/auth', { useNewUrlParser: true });
 //App setup
 const app = express();
 app.use(morgan('combined'));
+app.use(expressCors({
+  allowedOrigins: ['localhost:3000']
+}));
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
 //Server setup
 const port = process.env.PORT || 8000;
-const server = http.createServer(app); 
+const server = http.createServer(app);
 server.listen(port);
 console.log('Server running on port ' + port)
